a7f8691983dc233a59ff207a0b922d5accfc0c56
Sam Hopkins <samhop@google.com>
2012-08-17 10:45:23 -0700

    remove debugging prints

--- dart/tests/html/subclass_dom_elements_test.dart
+++ dart/tests/html/subclass_dom_elements_test.dart
@@ -10,30 +10,25 @@
 
 /** Test suite for subclassing of native elements. */
 main() {
-  print('beginning of main');
   useHtmlConfiguration();
 
   group('basic functionality', () {
     test('can construct instance of native subclass', () {
-      print('test 1');
       CustomDiv mydiv = new CustomDiv();
     });
 
     test('instance of native subclass has custom behavior', () {
-      print('test 2');
       CustomDiv mydiv = new CustomDiv();
       expect(mydiv.razzle(), equals('razzle'));
     });
 
     test('instance of native subclass has custom fields', () {
-      print('test 3');
       CustomDiv mydiv = new CustomDiv();
       mydiv.foo = 2;
       expect(mydiv.foo, equals(2));
     });
 
     test('instance of native subclass has native behavior', () {
-      print('test 4');
       CustomDiv mydiv = new CustomDiv();
       mydiv.innerHTML = '<p>foo</p>';
       expect(mydiv.nodes.length, equals(1));
@@ -45,23 +40,18 @@ main() {
     });
 
     test('custom elements behave well w.r.t. is-checks', () {
-      print('test 5');
       CustomDiv mydiv = new CustomDiv();
       expect(mydiv is CustomDiv, isTrue);
       expect(mydiv is DivElement);
     });
 
     test('queries retrieve custom elements', () {
-      print('test 6');
       CustomDiv mydiv = new CustomDiv();
       DivElement normaldiv = new DivElement();
       normaldiv.nodes.add(mydiv);
       expect(mydiv == normaldiv.query('div'));
-      print('end of test 6');
     });
-    print('end of group');
   });
-  print('finished test group');
 }
 
 class CustomDiv extends DivElementImpl {
